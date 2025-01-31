1616c625e31048822321b1567ce37f546475b989
EmissaryOfVddd <vdddenjoyer@gmail.com>
2025-01-31 10:23:04 +1000

    better logs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use nalgebra::{DMatrix, DVector, Matrix3, Vector3};
+use nalgebra::{DMatrix, DVector};
 use scalar::get_scalar_v;
 use strategies::{check_xy, get_x, get_y};
 use submatrices::extract_submatrices;
@@ -32,6 +32,44 @@ enum IfType {
     EndIteration,
 }
 
+// Функция для поиска седловых точек
+fn saddle_points(matrix: &DMatrix<f64>) -> (Vec<f64>, Vec<f64>) {
+    debug_info(Functions::SaddlePoints, IfType::Start, "Поиск седловых точек");
+
+    // Находим минимальные значения в строках
+    let mins_in_rows: Vec<f64> = matrix.row_iter()
+        .map(|row| row.min())
+        .collect();
+
+    // Находим индекс максимального из минимальных значений
+    let max_of_mins = mins_in_rows.iter().map(|&n| n).reduce(f64::min).unwrap();
+    let max_of_mins_index = mins_in_rows.iter().position(|&x| x == max_of_mins).unwrap();
+
+    // Создаем первый вектор
+    let mut raw_first_vector = vec![0.; matrix.nrows()];
+    raw_first_vector[max_of_mins_index] = 1.0;
+
+    // Транспонируем матрицу
+    let transposed_matrix = matrix.transpose();
+
+    // Находим максимальные значения в столбцах (строках транспонированной матрицы)
+    let maxs_in_columns: Vec<f64> = transposed_matrix.row_iter()
+    .map(|row| row.max())
+        .collect();
+
+    // Находим индекс минимального из максимальных значений
+    let min_of_maxs = maxs_in_columns.iter().map(|&n| n).reduce(f64::max).unwrap();
+    let min_of_maxs_index = maxs_in_columns.iter().position(|&x| x == min_of_maxs).unwrap();
+
+    // Создаем второй вектор
+    let mut raw_second_vector = vec![0.; matrix.nrows()];
+    raw_second_vector[min_of_maxs_index] = 1.0;
+
+    debug_info(Functions::SaddlePoints, IfType::Stop, &format!("Седловые точки найдены: {raw_first_vector:?}, {raw_second_vector:?}"));
+    (raw_first_vector, raw_second_vector)
+}
+
+
 fn main() {
     let m = DMatrix::from_columns(&[
         DVector::from_vec(vec![3., 5., 3.]),
@@ -39,7 +77,14 @@ fn main() {
         DVector::from_vec(vec![3., 2., 3.]),
     ]);
 
-    dbg!(get_results(&m));
+    // dbg!(get_results(&m));
+    let saddle_points = saddle_points(&m);
+    let results = get_results(&m);
+
+    for res in results {
+        println!("Ɑ({:.2?}) + (1 - Ɑ)({:.2?}), 0 ≤ Ɑ ≤ 1", saddle_points.0, res.0);
+        println!("β({:.2?}) + (1 - β)({:.2?}), 0 ≤ Ɑ ≤ 1", saddle_points.1, res.1);
+    }
 }
 
 fn get_results(matrix: &DMatrix<f64>) -> Vec<(Vec<f64>, Vec<f64>)> {
@@ -52,14 +97,14 @@ fn get_results(matrix: &DMatrix<f64>) -> Vec<(Vec<f64>, Vec<f64>)> {
             let combination = &combinations[index];
 
             if debug_info(Functions::Main, IfType::Info, "Submatrix:") {
-                println!("{:?}", submatrix);
+                println!("{:.2}", submatrix);
             }
             debug_info(Functions::Main, IfType::Info, &format!("Combination: {:?} \n", combination));
 
             let inv_submatrix = submatrix.clone().try_inverse().expect("Cannot invert submatrix");
 
             if debug_info(Functions::Main, IfType::Info, "Inverted Submatrix:") {
-                println!("{:?}", inv_submatrix);
+                println!("{:.2}", inv_submatrix);
             }
 
             let v;
@@ -86,7 +131,7 @@ fn get_results(matrix: &DMatrix<f64>) -> Vec<(Vec<f64>, Vec<f64>)> {
             }
 
             results.push((x, y));
-            debug_info(Functions::Main, IfType::EndIteration, "");
+            debug_info(Functions::Main, IfType::EndIteration, "Добавляем результаты");
         }
     }
 
